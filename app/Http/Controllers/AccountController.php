6da544f51a2f6c7e99830ddbdc3c0c32ccb9d113
Revand <revandaustin@gmail.com>
2023-04-03 20:11:13 +0700

    minor fixes

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('account.index', [
+            'user' => auth()->user()
+        ]);
+    }
+
+    public function edit(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|min:4|max:255',
+            'username' => 'required|unique:users|min:4',
+            'email' => 'required|email:dns'
+        ]);
+
+
+        User::where('username', auth()->user()->username)->update($validatedData);
+
+        return redirect('/account/' . auth()->user()->username)->with('success', 'Account updated');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CartController;
 use App\Models\Cart;
 use App\Models\User;
@@ -30,44 +31,41 @@ Route::get('/', function () {
 });
 
 // ! User login and registreation
-Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->middleware('guest');
-Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
+Route::get('/login', [LoginController::class, 'index'])
+->middleware('guest')->name('login');
 
-Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
-Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])
+->middleware('guest');
+
+Route::post('/logout', [LoginController::class, 'logout'])
+->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'index'])
+->middleware('guest');
+
+Route::post('/register', [RegisterController::class, 'store'])
+->middleware('guest');
 
 // ! Cart
-Route::get('/cart', [CartController::class, 'index'])->middleware('auth')->name('cart');
+Route::get('/cart', [CartController::class, 'index'])
+->middleware('auth')->name('cart');
 
-Route::put('/cart',  [CartController::class, 'edit'])->middleware('auth');
+Route::put('/cart',  [CartController::class, 'edit'])
+->middleware('auth');
 
-Route::delete('/cart', [CartController::class, 'destroy'])->middleware('auth');
+Route::delete('/cart', [CartController::class, 'destroy'])
+->middleware('auth');
 
 
 // ! Account
-Route::get('/account/{user}', function () {
-    return view('account.index', [
-        'user' => auth()->user()
-    ]);
-})->middleware('auth');
+Route::get('/account/{user}', [AccountController::class, 'show'])
+->middleware('auth');
 
-
-
-Route::put('account/{user:username}', function (IlluRequest $request, User $username) {
-    $validatedData = $request->validate([
-        'name' => 'required|min:4|max:255',
-        'username' => 'required|unique:users|min:4',
-        'email' => 'required|email:dns'
-    ]);
-
-
-    User::where('username', auth()->user()->username)->update($validatedData);
-
-    return redirect('/account/' . auth()->user()->username)->with('success', 'Account updated');
-})->middleware('auth');
+Route::put('account/{user:username}', [AccountController::class, 'edit'])
+->middleware('auth');
 
 // ! Product
 Route::get('/product/{product:slug}', [ProductController::class, 'show']);
 
-Route::post('/product', [ProductController::class, 'store'])->middleware('auth');
+Route::post('/product', [ProductController::class, 'store'])
+->middleware('auth');
